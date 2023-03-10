4262526f797562ebbef6fba81721b5fa4002d1fa
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-03-09 21:22:48 -0600

    Adds and renames sql packages
    - Adds seeds.sql file
    - Renames employees.sql to `main.sql`.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Adds values to department. 
+INSERT INTO department (name)
+VALUES 
+
+-- Adds values to roles
+INSERT INTO role (title, salary, department_id)
+
+-- Adds values to employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
